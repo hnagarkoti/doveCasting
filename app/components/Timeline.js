10963aa029e7114084d2ffe74ee014830e5a9e67
vijaya <vijaya@teampumpkin.com>
2016-12-26 16:03:07 +0530

    fix: UI chnages for timeline

--- app/components/Timeline.js
+++ app/components/Timeline.js
@@ -1,6 +1,6 @@
 import React from 'react';
-// import timelineStyle from '../styles/timeline.css'
-require('../styles/timeline.css');
+import timelineStyle from '../styles/timeline.css'
+// require('../styles/timeline.css');
 require('../js/timline.js');
 export default class Timeline extends React.Component {
   constructor(props) {
@@ -9,69 +9,35 @@ export default class Timeline extends React.Component {
   render(){
     return(
       <div>
-      <div className="stepwizard col-md-offset-3">
-    <div className="stepwizard-row setup-panel">
-      <div className="stepwizard-step">
-        <a href="#step-1" type="button" className="btn btn-primary btn-circle">1</a>
-        <p>Step 1</p>
-      </div>
-      <div className="stepwizard-step">
-        <a href="#step-2" type="button" className="btn btn-default btn-circle" disabled="disabled">2</a>
-        <p>Step 2</p>
-      </div>
-      <div className="stepwizard-step">
-        <a href="#step-3" type="button" className="btn btn-default btn-circle" disabled="disabled">3</a>
-        <p>Step 3</p>
-      </div>
-    </div>
-  </div>
-
-  <form role="form" action="" method="post">
-    <div className="row setup-content" id="step-1">
-      <div className="col-xs-6 col-md-offset-3">
-        <div className="col-md-12">
-          <h3> Step 1</h3>
-          <div className="form-group">
-            <label className="control-label">First Name</label>
-            <input  maxlength="100" type="text" required="required" className="form-control" placeholder="Enter First Name"  />
+        <div className={timelineStyle.stepwizard}>
+          <div className={timelineStyle['stepwizard-row setup-panel']}>
+            <div className={timelineStyle['stepwizard-step']}>
+              <a href="#step-1" type="button" className={timelineStyle['btn-primary btn-circle']}>1</a>
+              <p>Step 1</p>
+            </div>
+            <div className={timelineStyle['stepwizard-step']}>
+              <a href="#step-2" type="button" className={timelineStyle['btn-default btn-circle']}
+              disabled="disabled">2</a>
+              <p>Step 2</p>
+            </div>
+            <div className={timelineStyle['stepwizard-step']}>
+              <a href="#step-3" type="button" className={timelineStyle['btn-default btn-circle']} 
+              disabled="disabled">3</a>
+              <p>Step 3</p>
+            </div>
           </div>
-          <div className="form-group">
-            <label className="control-label">Last Name</label>
-            <input maxlength="100" type="text" required="required" className="form-control" placeholder="Enter Last Name" />
-          </div>
-          <div className="form-group">
-            <label className="control-label">Address</label>
-            <textarea required="required" className="form-control" placeholder="Enter your address" ></textarea>
-          </div>
-          <button className="btn btn-primary nextBtn btn-lg pull-right" type="button" >Next</button>
         </div>
-      </div>
-    </div>
-    <div className="row setup-content" id="step-2">
-      <div className="col-xs-6 col-md-offset-3">
-        <div className="col-md-12">
-          <h3> Step 2</h3>
-          <div className="form-group">
-            <label className="control-label">Company Name</label>
-            <input maxlength="200" type="text" required="required" className="form-control" placeholder="Enter Company Name" />
-          </div>
-          <div className="form-group">
-            <label className="control-label">Company Address</label>
-            <input maxlength="200" type="text" required="required" className="form-control" placeholder="Enter Company Address"  />
-          </div>
-          <button className="btn btn-primary nextBtn btn-lg pull-right" type="button" >Next</button>
+        <form role="form" action="" method="post">
+        <div className="row setup-content" id="step-1">
+        <h3> Step 1</h3>
         </div>
-      </div>
-    </div>
-    <div className="row setup-content" id="step-3">
-      <div className="col-xs-6 col-md-offset-3">
-        <div className="col-md-12">
-          <h3> Step 3</h3>
-          <button className="btn btn-success btn-lg pull-right" type="submit">Submit</button>
+        <div className="row setup-content" id="step-2">
+        <h3> Step 1</h3>
         </div>
-      </div>
-    </div>
-  </form>
+        <div className="row setup-content" id="step-3">
+        <h3> Step 1</h3>
+        </div>
+        </form>
       </div>
     )
   }
